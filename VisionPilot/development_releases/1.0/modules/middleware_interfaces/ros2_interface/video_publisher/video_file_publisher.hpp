9a819d454d44a2f73b66e98450897e96bd5f11ce
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-04 17:59:55 +0900

    add video_file_publisher header into corresponding module

---
+++ VisionPilot/development_releases/1.0/modules/middleware_interfaces/ros2_interface/video_publisher/video_file_publisher.hpp
@@ -0,0 +1,8 @@
+#ifndef VISIONPILOT_VIDEO_FILE_PUBLISHER_HPP
+#define VISIONPILOT_VIDEO_FILE_PUBLISHER_HPP
+
+namespace video_publisher {
+
+}; // namespace video_publisher
+
+#endif // VISIONPILOT_VIDEO_FILE_PUBLISHER_HPP
