0b1e47b914424666607f16273a3fe94dade080e7
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-11 10:43:17 +0900

    finalize reading from camera

--- VisionPilot/development_releases/1.0/modules/sensing/v4l2_interface/src/v4l2_reader.cpp
+++ VisionPilot/development_releases/1.0/modules/sensing/v4l2_interface/src/v4l2_reader.cpp
@@ -21,4 +21,27 @@ namespace v4l2_interface {
         log_info("  Device Path: " + device_path);
         log_info("  Target FPS: " + std::to_string(fps));
 
+        stats.device_path = device_path;
+        stats.current_fps = fps;
+
+        // Init display rate throttling
+        display_frame_period_ms = (fps > 0) ? (1000 / fps) : 33;
+        last_display_time = std::chrono::steady_clock::now();
+
+        // Parse device number from path (for example, "/dev/video0" => 0)
+        int device_number = 0;
+        try {
+            size_t last_slash = device_path.find_last_of('/');
+            if (last_slash != std::string::npos) {
+                std::string dev_num_str = device_path.substr(last_slash + 1);
+                if (dev_num_str.find("video") != std::string::npos) {
+                    device_number = std::stoi(dev_num_str.substr(5));
+                }
+            }
+        } catch (const std::exception& e) {
+            log_warning("Could not parse device number from path: " + device_path);
+        }
+
+        
+
 }
