d9e01880fca2241aac0c915183150805d85696e3
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-18 14:52:07 +0900

    exec: initial imports

--- VisionPilot/development_releases/1.0/modules/visualization/src/visualization_to_webrtc.cpp
+++ VisionPilot/development_releases/1.0/modules/visualization/src/visualization_to_webrtc.cpp
@@ -1,1 +1,16 @@
 #include <visualization/visualization_to_webrtc.hpp>
+
+// GStreamer headers for WebRTC streaming
+#include <gst/app/gstappsrc.h>
+#include <gst/sdp/gstsdpmessage.h>
+#include <gst/webrtc/webrtc.h>
+
+// libsoup for WebSocket signaling
+#include <libsoup/soup.h>
+
+// JSON-GLib for JSON handling in signaling messages
+#include <json-glib/json-glib.h>
+
+#include <algorithm>
+#include <cmath>
+#include <string>
